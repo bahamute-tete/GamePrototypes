14ca8c3b2a5d6b674328b65a52856efcb25ee71b
bahamute-tete <55549976+bahamute-tete@users.noreply.github.com>
2026-08-06 09:15:00 +0800

    去掉无用的命名空间

--- Assets/Funny/RayMarchingShader/Scripts/RayBoxIntersectionCheck.cs
+++ Assets/Funny/RayMarchingShader/Scripts/RayBoxIntersectionCheck.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class RayBoxIntersectionCheck : MonoBehaviour
